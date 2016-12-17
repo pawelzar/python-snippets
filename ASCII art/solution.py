bf2a66a5bafe42bac28b9c314cc44f69a2d07d9c
Paweł <ppjzarebski@gmail.com>
2016-12-17 09:40:13 +0100

    Added tests for ASCII art

--- ASCII art/solution.py
+++ ASCII art/solution.py
@@ -1,17 +1,34 @@
-l = int(input())
-h = int(input())
-text = input().upper()
+def ascii_art(width, height, text, art):
+    """Returns text represented in ASCII art."""
+    text_in_art = [""] * height
 
-art = [input() for i in range(h)]
-output = [""] * h
+    for sign in text.upper():
+        if ord(sign) in range(65, 91):
+            for line in range(height):
+                start = (ord(sign)-65) * width
+                end = ((ord(sign) - 65) + 1) * width
+                text_in_art[line] += art[line][start: end]
+        else:
+            for line in range(height):
+                text_in_art[line] += art[line][26 * width: 27 * width]
 
-for char in text:
-    if ord(char) in range(65, 91):
-        for line in range(h):
-            output[line] += art[line][(ord(char)-65)*l: ((ord(char)-65)+1)*l]
-    else:
-        for line in range(h):
-            output[line] += art[line][26*l: 27*l]
+    return text_in_art
 
-for row in output:
-    print(row)
+if __name__ == "__main__":
+    l = int(input())
+    h = int(input())
+    text = input().upper()
+
+    art = [input() for i in range(h)]
+    output = [""] * h
+
+    for char in text:
+        if ord(char) in range(65, 91):
+            for line in range(h):
+                output[line] += art[line][(ord(char)-65)*l: ((ord(char)-65)+1)*l]
+        else:
+            for line in range(h):
+                output[line] += art[line][26*l: 27*l]
+
+    for row in output:
+        print(row)
---
+++ ASCII art/tests.py
@@ -0,0 +1,67 @@
+import unittest
+from solution import ascii_art
+
+
+class AsciiArtTest(unittest.TestCase):
+    def setUp(self):
+        self.art = [
+            " #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ",
+            "# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # ",
+            "### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## ",
+            "# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       ",
+            "# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  "
+        ]
+
+    def test_one_sign(self):
+        result = [
+            "### ",
+            "#   ",
+            "##  ",
+            "#   ",
+            "### "
+        ]
+
+        self.assertEqual(ascii_art(4, 5, 'E', self.art), result,
+                         "Should return letter 'E' in ASCII art.")
+
+        result = [
+            "### ",
+            "  # ",
+            " ## ",
+            "    ",
+            " #  "
+        ]
+
+        self.assertEqual(ascii_art(4, 5, '?', self.art), result,
+                         "Should return sign '?' in ASCII art.")
+
+    def test_manhattan(self):
+        result = [
+            "# #  #  ### # #  #  ### ###  #  ### ",
+            "### # # # # # # # #  #   #  # # # # ",
+            "### ### # # ### ###  #   #  ### # # ",
+            "# # # # # # # # # #  #   #  # # # # ",
+            "# # # # # # # # # #  #   #  # # # # ",
+        ]
+
+        self.assertEqual(ascii_art(4, 5, 'MANHATTAN', self.art), result,
+                         "Should return 'MANHATTAN' in ASCII art.")
+
+        self.assertEqual(ascii_art(4, 5, 'ManhAtTan', self.art), result,
+                         "Should recognize lower and upper case.")
+
+    def test_manhattan_mixed(self):
+        result = [
+            "# # ### ### # # ### ### ### ### ### ",
+            "###   # # # # #   #  #   #    # # # ",
+            "###  ## # # ###  ##  #   #   ## # # ",
+            "# #     # # # #      #   #      # # ",
+            "# #  #  # # # #  #   #   #   #  # # "
+        ]
+
+        self.assertEqual(ascii_art(4, 5, 'M@NH@TT@N', self.art), result,
+                         "Should place '?' for '@' in M@NH@TT@N.")
+
+
+if __name__ == '__main__':
+    unittest.main()

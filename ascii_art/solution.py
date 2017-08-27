55b2dc288c08ed89bbcd2ce04e620317889f7465
Paweł Zarębski <pawel.zarebski@stxnext.com>
2017-08-27 22:15:33 +0200

    Add docstring to ascii_art and improve tests

--- ascii_art/solution.py
+++ ascii_art/solution.py
@@ -1,5 +1,13 @@
-def ascii_art(width, height, text, art):
-    """Returns text represented in ASCII art."""
+def ascii_art(width, height, art, text):
+    """
+    Represent text in ASCII art.
+
+    :param width: width of single element in ASCII art
+    :param height: height of single element in ASCII art
+    :param art: alphabet in ASCII art represented as several lines of strings
+    :param text: string that is going to be represented in ASCII art
+    :return: text converted to list of strings in ASCII art
+    """
     text_in_art = [''] * height
 
     for sign in text.upper():
@@ -7,17 +15,19 @@ def ascii_art(width, height, text, art):
             for line in range(height):
                 start = (ord(sign) - 65) * width
                 end = ((ord(sign) - 65) + 1) * width
-                text_in_art[line] += art[line][start: end]
+                text_in_art[line] += art[line][start:end]
         else:
             for line in range(height):
-                text_in_art[line] += art[line][26 * width: 27 * width]
+                # represent character as '?' (last sign in provided art)
+                # if it wasn't recognized
+                text_in_art[line] += art[line][26 * width:]
 
     return text_in_art
 
 if __name__ == '__main__':
-    l = int(input())
-    h = int(input())
+    width = int(input())
+    height = int(input())
     text = input()
-    art = [input() for i in range(h)]
+    art = [input() for i in range(height)]
 
-    print('\n'.join(ascii_art(l, h, text, art)))
+    print('\n'.join(ascii_art(width, height, art, text)))
--- ascii_art/tests.py
+++ ascii_art/tests.py
@@ -20,6 +20,8 @@ class AsciiArtTest(unittest.TestCase):
             '#     # # # ##   #  ###  #  # # # #  #  ###  #  ',
         ]
 
+        self.width = 4
+        self.height = 5
         self.art = [a + b for a, b in zip(part_a_o, part_p_z)]
 
     def test_one_sign(self):
@@ -31,8 +33,10 @@ class AsciiArtTest(unittest.TestCase):
             '### '
         ]
 
-        self.assertEqual(ascii_art(4, 5, 'E', self.art), result,
-                         'Should return letter \'E\' in ASCII art.')
+        self.assertEqual(
+            ascii_art(self.width, self.height, self.art, 'E'), result,
+            'Should return letter \'E\' in ASCII art.'
+        )
 
         result = [
             '### ',
@@ -42,8 +46,10 @@ class AsciiArtTest(unittest.TestCase):
             ' #  '
         ]
 
-        self.assertEqual(ascii_art(4, 5, '?', self.art), result,
-                         'Should return sign \'?\' in ASCII art.')
+        self.assertEqual(
+            ascii_art(self.width, self.height, self.art, '?'), result,
+            'Should return sign \'?\' in ASCII art.'
+        )
 
     def test_manhattan(self):
         result = [
@@ -54,11 +60,15 @@ class AsciiArtTest(unittest.TestCase):
             '# # # # # # # # # #  #   #  # # # # ',
         ]
 
-        self.assertEqual(ascii_art(4, 5, 'MANHATTAN', self.art), result,
-                         'Should return \'MANHATTAN\' in ASCII art.')
+        self.assertEqual(
+            ascii_art(self.width, self.height, self.art, 'MANHATTAN'), result,
+            'Should return \'MANHATTAN\' in ASCII art.'
+        )
 
-        self.assertEqual(ascii_art(4, 5, 'ManhAtTan', self.art), result,
-                         'Should recognize lower and upper case.')
+        self.assertEqual(
+            ascii_art(self.width, self.height, self.art, 'ManhAtTan'), result,
+            'Should recognize lower and upper case.'
+        )
 
     def test_manhattan_mixed(self):
         result = [
@@ -69,8 +79,10 @@ class AsciiArtTest(unittest.TestCase):
             '# #  #  # # # #  #   #   #   #  # # '
         ]
 
-        self.assertEqual(ascii_art(4, 5, 'M@NH@TT@N', self.art), result,
-                         'Should place \'?\' for \'@\' in M@NH@TT@N.')
+        self.assertEqual(
+            ascii_art(self.width, self.height, self.art, 'M@NH@TT@N'), result,
+            'Should place \'?\' for \'@\' in M@NH@TT@N.'
+        )
 
 
 if __name__ == '__main__':
